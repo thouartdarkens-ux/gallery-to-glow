1a71cf558c61c2910f3ca33b79dff098ca098b99
thouartdarkens-ux <thouartdarkens@gmail.com>
2025-11-06 10:20:46 +0000

    Updated LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ const LandingPage = () => {
   return (
     <div 
       className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: 'url(/background.jpg)' }}
+      style={{ backgroundImage: 'url(/Background%20(1).jpg)' }}
     >
     </div>
   );
